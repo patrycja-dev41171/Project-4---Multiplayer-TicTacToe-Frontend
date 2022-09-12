230f9af0469b705a6a80b7987ef125ad873d306e
Patrycja Ińska <patrycja.inska@interia.pl>
2022-09-12 22:07:28 +0200

    Adding Routing.

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,17 @@ import "./index.css";
 import { App } from "./App";
 import { ThemeProvider } from "@mui/material/styles";
 import {mainTheme} from "./utils/materialUITheme";
+import {BrowserRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
+      <BrowserRouter>
     <ThemeProvider theme={mainTheme}>
       <App />
     </ThemeProvider>
+      </BrowserRouter>
   </React.StrictMode>
 );
