2c1b7f42538cdde970ec4ee2e1eee969c2d43c9e
Patrycja Ińska <patrycja.inska@interia.pl>
2022-09-14 23:53:38 +0200

    Creating a Home component for the Home page. Extracting smaller components into separate files. Adding css styles.

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import { Login } from "./components/Login/Login";
 import { SignUp } from "./components/SignUp/SignUp";
 import { Welcome } from "./components/Welcome/Welcome";
+import {Home} from "./components/Home/Home";
 
 export const App = () => {
   return (
@@ -10,7 +11,8 @@ export const App = () => {
       <Routes>
         <Route path="/" element={<Welcome />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/home" element={<Home />} />
       </Routes>
     </div>
   );
