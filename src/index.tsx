337c293f2fc937e242380213bdff4ad25077495f
Patrycja Ińska <patrycja.inska@interia.pl>
2022-09-19 22:14:13 +0200

    Added React.StrictMode.

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "@mui/material/styles";
 import { mainTheme } from "./utils/materialUITheme";
-import { Provider } from 'react-redux';
-import { store } from './redux-toolkit/store';
+import { Provider } from "react-redux";
+import { store } from "./redux-toolkit/store";
 import "./socket-io/socket";
 import { App } from "./App";
 import "./index.css";
@@ -13,13 +13,13 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  // <React.StrictMode>
-  <BrowserRouter>
-    <Provider store={store}>
-      <ThemeProvider theme={mainTheme}>
-        <App />
-      </ThemeProvider>
-    </Provider>
-  </BrowserRouter>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <ThemeProvider theme={mainTheme}>
+          <App />
+        </ThemeProvider>
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
